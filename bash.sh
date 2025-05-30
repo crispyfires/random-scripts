e212414b8f9895e7d2b0ac2deb94bd3bd3b70553
crispyfires <152764922+crispyfires@users.noreply.github.com>
2025-05-30 14:53:14 -0400

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash
 #Flatpak
-flatpak install zeroad anki bottles boxbuddy deltapatcher discord eloquent errands evolution errands flatseal flatsweep warehouse moonlight flips gearlever gimp libreoffice collision heroic lemonade libreoffice limo lorem melonsd mgba sameboy missioncenter newsflash nyxt planify polari prismlauncher qutebrowser randomnumberfive  retroarch sameboy tuxpaint tuxtyping ungoogledchromium epiphany whatsie winezgui zen zotero decoder readitlater obfuscate contrast serigy embellish upscaler archives tuba keypunch raider junction chance converter impression vscodium freetube curtail runemaster savedesktop stimulator citations varia dejadup wike tangram firefox
+flatpak install zeroad anki bottles boxbuddy deltapatcher discord eloquent errands evolution errands flatseal flatsweep warehouse moonlight flips gearlever gimp libreoffice collision heroic webapps lemonade libreoffice limo lorem melonsd mgba sameboy missioncenter newsflash nyxt planify polari prismlauncher qutebrowser randomnumberfive  retroarch sameboy tuxpaint tuxtyping ungoogledchromium epiphany whatsie winezgui zen zotero decoder readitlater obfuscate contrast serigy embellish upscaler archives tuba keypunch raider junction chance converter impression vscodium freetube curtail runemaster savedesktop stimulator citations varia dejadup wike tangram firefox
 #Neovim
 LV_BRANCH='release-1.4/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh)
